dcee80738ede026aac70f2a569465697a653fcdd
aashrithareddy595 <aashrithareddy595@gmail.com>
2024-07-04 01:39:38 +0530

    another way of writing variable

---
+++ app/src/main/java/com/example/variables/newvariables/three.kt
@@ -0,0 +1,7 @@
+package com.example.variables.newvariables
+
+fun main() {
+    val pendingWork = 10
+    val completedWork = 30
+    println("You have ${pendingWork + completedWork} works in total .")
+}
