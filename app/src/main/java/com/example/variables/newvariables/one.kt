560d40ccd1f1b0629f0f5061ca599643ff47c480
aashrithareddy595 <aashrithareddy595@gmail.com>
2024-07-04 01:31:28 +0530

    flexibilty in string

---
+++ app/src/main/java/com/example/variables/newvariables/one.kt
@@ -0,0 +1,6 @@
+package com.example.variables.newvariables
+
+fun main() {
+    val count: Int = 4
+    println(count)
+}
---
+++ app/src/main/java/com/example/variables/newvariables/two.kt
@@ -0,0 +1,6 @@
+package com.example.variables.newvariables
+
+fun main() {
+    val xyz: Int = 20
+    println("You have $xyz missed calls from your family and friends.")
+}
