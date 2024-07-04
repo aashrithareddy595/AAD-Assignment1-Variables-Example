76bd4f46c3aa4dc9abb0b5488f430de892d955fa
aashrithareddy595 <aashrithareddy595@gmail.com>
2024-07-04 23:17:06 +0530

    var is updated

---
+++ app/src/main/java/com/example/variables/newvariables/varandvalupdates.kt
@@ -0,0 +1,13 @@
+package com.example.variables.newvariables
+
+//fun main() {
+//    val design = 6
+////    design = 10
+////    gives error
+//    println("Total: $design")
+//}
+fun main() {
+    var design = 0
+    design = 10
+    println("Total: $design")
+}
