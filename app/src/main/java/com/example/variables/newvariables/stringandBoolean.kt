3b76b748a16b907883cf6c83c3b822f327cb7910
aashrithareddy595 <aashrithareddy595@gmail.com>
2024-07-05 14:37:23 +0530

    String and boolean examples

---
+++ app/src/main/java/com/example/variables/newvariables/stringandBoolean.kt
@@ -0,0 +1,10 @@
+package com.example.variables.newvariables
+
+fun main() {
+    val newproject = "New Project: "
+    val deadlinedate = "July 5"
+    val reminder = newproject + deadlinedate + " is your deadline"
+    println(reminder)
+    val notificationsEnabled: Boolean = true
+    println(notificationsEnabled)
+}
